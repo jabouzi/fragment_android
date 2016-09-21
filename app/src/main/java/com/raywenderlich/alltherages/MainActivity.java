961c6b588c572115fcc41906427ef897fa3879c2
Skander Jabouzi <skander.jabouzi@geemedia.com>
2016-09-21 17:08:50 -0400

    first commit

---
+++ app/src/main/java/com/raywenderlich/alltherages/MainActivity.java
@@ -0,0 +1,32 @@
+package com.raywenderlich.alltherages;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+    implements RageComicListFragment.OnRageComicSelected {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    if (savedInstanceState == null) {
+      getSupportFragmentManager()
+          .beginTransaction()
+          .replace(R.id.root_layout, RageComicListFragment.newInstance(), "rageComicList")
+          .commit();
+    }
+  }
+
+  @Override
+  public void onRageComicSelected(int imageResId, String name, String description, String url) {
+    final RageComicDetailsFragment detailsFragment =
+        RageComicDetailsFragment.newInstance(imageResId, name, description, url);
+    getSupportFragmentManager()
+        .beginTransaction()
+        .replace(R.id.root_layout, detailsFragment, "rageComicDetails")
+        .addToBackStack(null)
+        .commit();
+  }
+}
